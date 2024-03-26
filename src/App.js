5981484d433ef17e0a811a02454c9c6dd9168bbb
Yassin-Zaher <yassinzaherelfeliri@gmail.com>
2024-03-26 15:40:03 +0000

    V7

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Alawi
+          Edit <code>src/App.js</code> Alawi Habib Galbi.
         </p>
         <a
           className="App-link"
