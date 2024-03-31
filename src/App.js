b366cec7c352b813bf5887b11623cf630bf503fa
Yassin-Zaher <yassinzaherelfeliri@gmail.com>
2024-03-31 01:14:23 +0000

    added worflows

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello GIGD</h1>
+        <h3>Hello From Github Actions</h3>
         <a
           className="App-link"
           href="https://reactjs.org"
